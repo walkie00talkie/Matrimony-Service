85c16833935764b62eede465558976989472e052
walkytalky013 <walkytalky013@gmail.com>
2019-03-14 09:48:38 +0600

    Commit Staged

---
+++ login_new/Models/Extended/reg.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace login_new.Models
+{
+    [MetadataType(typeof(usermetadata))]
+    public partial class reg
+    {
+
+    }
+
+    public class usermetadata
+    {
+        [Display(Name = "Id")]
+
+        [Required(AllowEmptyStrings = false , ErrorMessage = "Id required")]
+        public int Id { get; set; }
+
+        [Display(Name = "pwd")]
+        [DataType(DataType.Password)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "password required")]
+        [MinLength(6,ErrorMessage = "Must be need 6 characters")]
+        public string pwd { get; set; }
+    }
+}
